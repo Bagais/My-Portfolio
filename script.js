f03434f8daaf605c7d37c1ded46563972b2d5489
Elisha Bagaiya <105201298+Bagais@users.noreply.github.com>
2022-07-12 22:24:08 +0100

    Better structure
    1. fine-tuned the general display of landing page to give it better style.
    
    2. Created a set of charts to show competencies using JavaScript

--- script.js
+++ script.js
@@ -4,3 +4,75 @@ function fourthCategory() {
 	document.getElementById("fourth").style.backgroundImage = "url('images/20211013_145114.jpg')";
 }	
 fourthCategory()
+
+//FOR Web Dev Chart
+
+var xValues = [85, 65, 53, 5, 40];
+var yValues = ['HTML', 'CSS', 'JavaScript', 'React', 'Python'];
+var barColors = ['red', 'blue', 'yellow', 'green', 'brown'];
+
+new Chart('webChart', {
+	type: "bar",
+	data: {
+		labels: yValues, 
+		datasets: [{
+			backgroundColor: barColors,
+			data: xValues
+		}]
+	},
+	options: {
+		legend: {display: false},
+		title: {
+			display: true,
+			text: 'Web Development'
+		}
+	}
+});
+
+// FOR CAD CHART
+
+var xValues = [95, 93, 95, 55, 50, 0];
+var yValues = ['CorelDraw', 'AutoCAD', 'SketchUp', 'Revit', 'TwinMotion', '.'];
+var barColors = ['red', 'blue', 'yellow', 'green', 'brown', 'purple'];
+
+new Chart('designChart', {
+	type: "bar",
+	data: {
+		labels: yValues, 
+		datasets: [{
+			backgroundColor: barColors,
+			data: xValues
+		}]
+	},
+	options: {
+		legend: {display: false},
+		title: {
+			display: true,
+			text: 'Computer-Aided Design'
+		}
+	}
+});
+
+// FOR MISC. CHART
+
+var xValues = [90, 80, 50, 40, 0];
+var yValues = ['Sketching', 'Painting', 'Illustration', 'Research', '.'];
+var barColors = ['red', 'blue', 'yellow', 'green'];
+
+new Chart('miscChart', {
+	type: "bar",
+	data: {
+		labels: yValues, 
+		datasets: [{
+			backgroundColor: barColors,
+			data: xValues
+		}]
+	},
+	options: {
+		legend: {display: false},
+		title: {
+			display: true,
+			text: 'Miscellaneous'
+		}
+	}
+});
